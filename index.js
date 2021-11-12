ac2f396e3b28864fc07b256447b4dd61a6cabe1f
Fred <higgins.fred.s@gmail.com>
2021-11-11 16:51:18 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+const message = [];
+
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++) {
+message.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+       
+    }
+
+    return message;
+
+}
+
+
+function countDown(number) {
+    let n = 10;
+    while (n >= 0) {
+        console.log(n);
+        
+        n--;
+    }
+    return n;
+}
+
