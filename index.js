b6ce7e84c0fec802b616cc313fd87c9b807a5112
Fred <higgins.fred.s@gmail.com>
2021-12-04 09:33:27 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -2,22 +2,18 @@ const message = [];
 
 function writeCards(names, event) {
     for (let i = 0; i < names.length; i++) {
-message.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
-       
+        message.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        
     }
-
     return message;
-
-}
-
+};
 
 function countDown(number) {
-    let n = 10;
-    while (n >= 0) {
-        console.log(n);
-        
-        n--;
+    while (number >= 0) {
+        console.log(number);
+        number--;
     }
-    return n;
+    return number;
 }
 
+console.log(countDown(4));
